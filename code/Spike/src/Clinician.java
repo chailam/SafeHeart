815e82bc7e871273664b20126cb7220acfb97e79
Chai Lam Loi <cloi0001@student.monash.edu>
2019-04-25 15:32:45 +1000

    Created class for UserRole, User, Clinician, Practitioner & Patient

--- code/Spike/src/Clinician.java
+++ code/Spike/src/Clinician.java
@@ -1,4 +1,18 @@
+/* Created by: Loi Chai Lam
+ * Modified Date: 25-4-2019
+ */
 
-public class Clinician {
 
+public class Clinician extends UserRole {
+	/* Clinician class is the parent class of Practitioner */
+
+	// Constructor
+	public Clinician(int id) {
+		super(id);
+	}
+		
+	//Constructor
+	public Clinician(int id, String familyName, String givenName, int age, String gender ) {
+		super(id, familyName, givenName, age, gender);
+	}
 }
--- code/Spike/src/Patient.java
+++ code/Spike/src/Patient.java
@@ -1,7 +1,18 @@
+/* Created by: Loi Chai Lam
+ * Modified Date: 25-4-2019
+ */
 
-public class Patient {
-	public Patient(){
-		// Do something
-	}
+public class Patient extends UserRole {
+	/* Patient class is the sub-class of UserRole. 
+	 * It records the details of Patient */
 
+	//Constructor
+		public Patient(int id) {
+			super(id);
+		}
+			
+		//Constructor
+		public Patient(int id, String familyName, String givenName, int age, String gender ) {
+			super(id, familyName, givenName, age, gender);
+		}
 }
