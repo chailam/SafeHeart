0a9e3560c73ec145a136e3b0769bd205c16e0462
angg0008 <angg0008@student.monash.edu>
2019-04-26 03:11:09 +1000

    Added public enum Resource

---
+++ SafeHeart/src/main/java/XD/SafeHeart/Resource.java
@@ -0,0 +1,19 @@
+/*
+ * FIT3077 Assignment 2 SafeHeart
+ * Made by:
+ *	 Aik Han Ng (28947991)
+ *	 Chai Lam Loi (28136179)
+ */
+package XD.SafeHeart;
+
+/**
+ * Used to denote resource
+ * @author Aik Han Ng <angg0008@student.monash.edu>
+ */
+public enum Resource {
+    PATIENT,
+    PRACTITIONER,
+    ENCOUNTER,
+    DIAGREPORT,
+    OBSERVATION,
+}
