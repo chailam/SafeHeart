3ef611f8e73012564cfd9d440c52fbfdf76cb768
angg0008 <angg0008@student.monash.edu>
2019-05-03 13:27:42 +1000

    Updated mainClassName

--- SafeHeart/src/main/java/xd/safeheart/system/FhirManager.java
+++ SafeHeart/src/main/java/xd/safeheart/system/FhirManager.java
@@ -10,9 +10,10 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.model.primitive.IdDt;
 import ca.uhn.fhir.rest.client.IGenericClient;
 import org.hl7.fhir.dstu3.model.*;
+import xd.safeheart.model.*;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.HashMap;
 
 /**
  *
@@ -22,6 +23,10 @@ public class FhirManager {
     private final String serverBaseUrl;
     private final FhirContext ctx;
     private final IGenericClient client;
+    private xd.safeheart.model.Practitioner practitioner;
+    private HashMap<String, xd.safeheart.model.Encounter> encounterMap;
+    private HashMap<String, xd.safeheart.model.Patient> patientMap;
+    private HashMap<String, xd.safeheart.model.Observation> observationMap;
     public FhirManager(String inputUrl) {
         serverBaseUrl = inputUrl;
         
@@ -37,107 +42,174 @@ public class FhirManager {
         // create the RESTful client to work with our FHIR server
         // see http://hapifhir.io/doc_rest_client.html
         client = ctx.newRestfulGenericClient(serverBaseUrl);
+        
+        //practitioner = null;
+        
+        encounterMap = new HashMap<String, xd.safeheart.model.Encounter>();
+        patientMap = new HashMap<String, xd.safeheart.model.Patient>();
+        observationMap = new HashMap<String, xd.safeheart.model.Observation>();
+        
     }
     
-    public void searchPatientById(String id) throws IOException {
+    public boolean populateDataByPractitionerId(String id) {
+        org.hl7.fhir.dstu3.model.Practitioner targetPractitioner = this.searchPractitionerById(id);
+        if(targetPractitioner == null){ return false; }
+        else {
+            // Store practitioner
+            System.out.println(targetPractitioner.getName().get(0).getGivenAsSingleString());
+            this.practitioner = new xd.safeheart.model.Practitioner(id, 
+                    targetPractitioner.getName().get(0).getFamilyAsSingleString(),
+                    targetPractitioner.getName().get(0).getGivenAsSingleString()
+            );
+            
+            Bundle encBundle = this.client.search()
+                    .forResource(org.hl7.fhir.dstu3.model.Encounter.class)
+                    .where(org.hl7.fhir.dstu3.model.Encounter.PARTICIPANT.hasId(id))
+                    .returnBundle(Bundle.class)
+                    .execute();
+            
+            System.out.println(encBundle.getTotal());
+            
+            encBundle.getEntry().forEach((entry) -> {
+                // within each entry is a resource
+                System.out.println(entry.getResource().getIdElement().getIdPart());
+                System.out.println(this.searchEncounterById(entry.getResource().getIdElement().getIdPart()).getSubject().getReference());
+                //this.encounterMap.put(entryId, this.searchEncounterById(id));
+            });
+            
+            // maybe make this a method
+            while (encBundle.getLink(Bundle.LINK_NEXT) != null) {
+                System.out.println("next-page");
+                // load next page
+                encBundle = client.loadPage().next(encBundle).execute();
+                encBundle.getEntry().forEach((entry) -> {
+                // within each entry is a resource
+                String entryId = entry.getResource().getIdElement().getIdPart();
+                System.out.println(entryId);
+                //this.encounterMap.put(entryId, this.searchEncounterById(entryId));
+                System.out.println(this.searchEncounterById(entryId).getSubject().getReference());
+            });
+            }
+            
+            // maybe use url $everything http://build.fhir.org/patient-operation-everything.html
+            
+            //System.out.println(this.encounterMap.get("9323").getId());
+                    
+            return true;
+        }
+    }
+    
+    private org.hl7.fhir.dstu3.model.Resource searchResourceByUrl(String url) {
+        return client.search()
+                .byUrl(url)
+                .returnBundle(Bundle.class)
+                .execute();
+    }
+    
+    private org.hl7.fhir.dstu3.model.Patient searchPatientById(String id) {
         try {
             /* Not bundle because it's unique response
              * src: https://nhsconnect.github.io/gpconnect/foundations_use_case_read_a_patient.html
             */
             // problem as name might collide with our patient
-            Patient responsePatient = this.client.read()
-                    .resource(Patient.class)
+            return this.client.read()
+                    .resource(org.hl7.fhir.dstu3.model.Patient.class)
                     .withId(id)
                     .execute();
-            System.out.println("pat Family name: " + responsePatient.getName().get(0).getFamily());
             // PROBLEM: how to get all enounters that has patient?
         } catch (Exception e) {
             System.out.println("An error occurred trying to search:");
             e.printStackTrace();
+            return null;
         }
     }
         
-    public void searchPractitionerById(String id) throws IOException {
+    private org.hl7.fhir.dstu3.model.Practitioner searchPractitionerById(String id) {
         try {
             /* Not bundle because it's unique response
              * src: https://nhsconnect.github.io/gpconnect/foundations_use_case_read_a_patient.html
             */
-            Practitioner responsePractitoner = this.client.read()
-                    .resource(Practitioner.class)
+            // will cause error when bundle size (results) = 0
+            return this.client.read()
+                    .resource(org.hl7.fhir.dstu3.model.Practitioner.class)
                     .withId(id)
                     .execute();
-            System.out.println("prac Family name: " + responsePractitoner.getName().get(0).getFamily());
-            System.out.println("prac Family name: " + responsePractitoner.getName().get(0).getFamily());
+//            System.out.println("prac Family name: " + responsePractitoner.getName().get(0).getFamily());
+//            System.out.println("prac Family name: " + responsePractitoner.getName().get(0).getFamily());
         } catch (Exception e) {
             System.out.println("An error occurred trying to search:");
             e.printStackTrace();
+            return null;
         }
     }
     
-    public void searchEncounterById(String id) throws IOException {
+    private org.hl7.fhir.dstu3.model.Encounter searchEncounterById(String id) {
         try {
             /* Not bundle because it's unique response
              * src: https://nhsconnect.github.io/gpconnect/foundations_use_case_read_a_patient.html
             */
-            Encounter responseEncounter = this.client.read()
-                    .resource(Encounter.class)
+            return this.client.read()
+                    .resource(org.hl7.fhir.dstu3.model.Encounter.class)
                     .withId(id)
                     .execute();
             
-            // Might be more than one practicioners
-            List<Encounter.EncounterParticipantComponent> practicionerComps = responseEncounter.getParticipant();
-            for (Encounter.EncounterParticipantComponent component : practicionerComps)
-            {
-                Reference practicionerRef = component.getIndividual();
-                // relative string url of reference
-                System.out.println("enc prac reference: " + practicionerRef.getReference());
-            }
-            Reference patientRef = responseEncounter.getSubject();
-            // relative string url of reference
-            System.out.println("enc patient reference: " + patientRef.getReference());
+//            // Might be more than one practicioners
+//            List<org.hl7.fhir.dstu3.model.Encounter.EncounterParticipantComponent> practicionerComps = responseEncounter.getParticipant();
+//            for (org.hl7.fhir.dstu3.model.Encounter.EncounterParticipantComponent component : practicionerComps)
+//            {
+//                Reference practicionerRef = component.getIndividual();
+//                // relative string url of reference
+//                System.out.println("enc prac reference: " + practicionerRef.getReference());
+//            }
+//            Reference patientRef = responseEncounter.getSubject();
+//            // relative string url of reference
+//            System.out.println("enc patient reference: " + patientRef.getReference());
         } catch (Exception e) {
             System.out.println("An error occurred trying to search:");
             e.printStackTrace();
+            return null;
         }
     }
     
-    public void searchDiagReportById(String id) throws IOException {
+    private org.hl7.fhir.dstu3.model.DiagnosticReport searchDiagReportById(String id) {
         try {
             /* Not bundle because it's unique response
              * src: https://nhsconnect.github.io/gpconnect/foundations_use_case_read_a_patient.html
             */
-            DiagnosticReport responseDiagReport = this.client.read()
-                    .resource(DiagnosticReport.class)
+            return this.client.read()
+                    .resource(org.hl7.fhir.dstu3.model.DiagnosticReport.class)
                     .withId(id)
                     .execute();
             
-            System.out.println("Diagreport patient reference: " + responseDiagReport.getSubject().getReference());
-            System.out.println("Diagreport encounter reference: " + responseDiagReport.getContext().getReference());
-            String display = String.format("Diagreport observer '%s' reference: ", responseDiagReport.getResult().get(0).getDisplay());
-            // getresult returns list of references to all observers
-            System.out.println(display + responseDiagReport.getResult().get(0).getReference());
+//            System.out.println("Diagreport patient reference: " + responseDiagReport.getSubject().getReference());
+//            System.out.println("Diagreport encounter reference: " + responseDiagReport.getContext().getReference());
+//            String display = String.format("Diagreport observer '%s' reference: ", responseDiagReport.getResult().get(0).getDisplay());
+//            // getresult returns list of references to all observers
+//            System.out.println(display + responseDiagReport.getResult().get(0).getReference());
         } catch (Exception e) {
             System.out.println("An error occurred trying to search:");
             e.printStackTrace();
+            return null;
         }
     }
     
-    public void searchObservationById(String id) throws IOException {
+    private org.hl7.fhir.dstu3.model.Observation searchObservationById(String id) {
         try {
             /* Not bundle because it's unique response
              * src: https://nhsconnect.github.io/gpconnect/foundations_use_case_read_a_patient.html
             */
-            Observation responseObservation = this.client.read()
-                    .resource(Observation.class)
+            return this.client.read()
+                    .resource(org.hl7.fhir.dstu3.model.Observation.class)
                     .withId(id)
                     .execute();
-            System.out.println("Observation patient reference: " + responseObservation.getSubject().getReference());
-            System.out.println("Observation encounter reference: " + responseObservation.getContext().getReference());
-            // refer to observation structure in server
-            System.out.format("Observation '%s' value: %s %s", responseObservation.getCode().getText(), responseObservation.getValueQuantity().getValue(), responseObservation.getValueQuantity().getUnit());
+//            System.out.println("Observation patient reference: " + responseObservation.getSubject().getReference());
+//            System.out.println("Observation encounter reference: " + responseObservation.getContext().getReference());
+//            // refer to observation structure in server
+//            System.out.format("Observation '%s' value: %s %s", responseObservation.getCode().getText(), responseObservation.getValueQuantity().getValue(), responseObservation.getValueQuantity().getUnit());
         } catch (Exception e) {
             System.out.println("An error occurred trying to search:");
             e.printStackTrace();
+            return null;
         }
     }
 }
